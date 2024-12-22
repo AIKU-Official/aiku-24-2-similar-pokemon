fc4310b119bf93cd983cdb49f574fc2cc1ab2203
dahyun <sandy2873@naver.com>
2024-12-22 16:13:20 +0000

    refactor

--- DINO/DINO.py
+++ DINO/DINO.py
@@ -10,7 +10,6 @@ from PIL import Image
 class DINO:
 	def __init__(self, index_path):
 		dinov2_vitb14 = torch.hub.load("facebookresearch/dinov2", "dinov2_vitb14")
-		# breakpoint()
 		faiss_index = faiss.read_index(index_path)
 		device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 		transform = T.Compose([
@@ -28,16 +27,11 @@ class DINO:
 
 
 	def __call__(self, query_img: Image, k: int = 3) -> Tuple:
-		"""
-			Conduct K nearest search on given image file.
-			Returns a tuple that contains (similarity scores, indexes) of k-nearest-neighbor search.
-		"""
 		# 검색할 유명인 이미지
 		with torch.no_grad():
 			image = self.transform_image(query_img).unsqueeze(0).to(self.device)
 			embedding = self.dino_model(image)
 			query_embedding = np.array(embedding[0].cpu()).reshape(1, -1)
 			faiss.normalize_L2(query_embedding)
-		# breakpoint()
 		distances, indices = self.faiss_index.search(query_embedding, k)
 		return (distances[0], indices[0])
